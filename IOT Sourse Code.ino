9ff7858ec5a50effda2fce98a5788e906ff665ad
Akshint0407 <166590337+Akshint0407@users.noreply.github.com>
2024-10-07 11:50:02 +0530

    Update IOT Sourse Code.ino
    made the code more readable and shorter for easier understandable purpose.

--- IOT Sourse Code.ino
+++ IOT Sourse Code.ino
@@ -1,25 +1,22 @@
-IOT Sourse Code
-//Prateek
-//www.prateeks.in
-
-
 #include <AltSoftSerial.h>
 #include <TinyGPS++.h>
-#include<Wire.h>
+#include <Wire.h>
 #include <math.h>
 #include <SoftwareSerial.h>
 
-const String EMERGENCY_PHONE = "+91xxxxxxxxxx";
+// Emergency contact phone number
+const String EMERGENCY_PHONE = "+91xxxxxxxxxx"; // Replace with actual number
+
 #define rxPin 2
 #define txPin 3
 
 SoftwareSerial sim800(rxPin, txPin);
 AltSoftSerial neogps;
 TinyGPSPlus gps;
-//gps(9,8)
 
 String sms_status, sender_number, received_date, msg;
 String latitude, longitude;
+
 #define BUZZER 12
 #define BUTTON 11
 #define xPin A1
@@ -27,7 +24,6 @@ String latitude, longitude;
 #define zPin A3
 
 byte updateflag;
-
 int xaxis = 0, yaxis = 0, zaxis = 0;
 int deltx = 0, delty = 0, deltz = 0;
 int vibration = 2;
@@ -35,25 +31,20 @@ int devibrate = 75;
 int magnitude = 0;
 int sensitivity = 20;
 
-double angle;
 boolean impact_detected = false;
 unsigned long time1;
 unsigned long impact_time;
 unsigned long alert_delay = 30000;
 
-void setup()
-{
-
+void setup() {
   Serial.begin(9600);
   sim800.begin(9600);
   neogps.begin(9600);
+  
   pinMode(BUZZER, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);
-
-  sms_status = "";
-  sender_number = "";
-  received_date = "";
-  msg = "";
+  
+  // Initialize SIM800
   sim800.println("AT");
   delay(1000);
   sim800.println("ATE1");
@@ -64,33 +55,29 @@ void setup()
   delay(1000);
   sim800.println("AT+CNMI=1,1,0,0,0");
   delay(1000);
+  
   time1 = micros();
   xaxis = analogRead(xPin);
   yaxis = analogRead(yPin);
   zaxis = analogRead(zPin);
 }
 
-
-
-void loop()
-{
+void loop() {
   if (micros() - time1 > 1999) Impact();
-  if (updateflag > 0)
-  {
+  
+  if (updateflag > 0) {
     updateflag = 0;
     Serial.println("Impact detected!!");
-    Serial.print("Magnitude:");
+    Serial.print("Magnitude: ");
     Serial.println(magnitude);
 
     getGps();
     digitalWrite(BUZZER, HIGH);
     impact_detected = true;
     impact_time = millis();
-
-
   }
-  if (impact_detected == true)
-  {
+
+  if (impact_detected) {
     if (millis() - impact_time >= alert_delay) {
       digitalWrite(BUZZER, LOW);
       makeCall();
@@ -107,16 +94,16 @@ void loop()
     impact_detected = false;
     impact_time = 0;
   }
+
   while (sim800.available()) {
     parseData(sim800.readString());
   }
-  while (Serial.available())  {
+  while (Serial.available()) {
     sim800.println(Serial.readString());
   }
 }
 
-void Impact()
-{
+void Impact() {
   time1 = micros();
   int oldx = xaxis;
   int oldy = yaxis;
@@ -126,37 +113,35 @@ void Impact()
   yaxis = analogRead(yPin);
   zaxis = analogRead(zPin);
 
-
   vibration--;
   Serial.print("Vibration = ");
   Serial.println(vibration);
+  
   if (vibration < 0) vibration = 0;
   if (vibration > 0) return;
+
   deltx = xaxis - oldx;
   delty = yaxis - oldy;
   deltz = zaxis - oldz;
 
-
   magnitude = sqrt(sq(deltx) + sq(delty) + sq(deltz));
-  if (magnitude >= sensitivity) //impact detected
-  {
+  
+  if (magnitude >= sensitivity) {
     updateflag = 1;
     vibration = devibrate;
-  }
-  else
-  {
+  } else {
     magnitude = 0;
   }
 }
 
-
 void parseData(String buff) {
   Serial.println(buff);
-
   unsigned int len, index;
+  
   index = buff.indexOf("\r");
   buff.remove(0, index + 2);
   buff.trim();
+  
   if (buff != "OK") {
     index = buff.indexOf(":");
     String cmd = buff.substring(0, index);
@@ -165,32 +150,25 @@ void parseData(String buff) {
     buff.remove(0, index + 2);
     if (cmd == "+CMTI") {
       index = buff.indexOf(",");
-      String temp = buff.substring(index + 1, buff.length());
+      String temp = buff.substring(index + 1);
       temp = "AT+CMGR=" + temp + "\r";
       sim800.println(temp);
-    }
-    else if (cmd == "+CMGR") {
+    } else if (cmd == "+CMGR") {
       if (buff.indexOf(EMERGENCY_PHONE) > 1) {
         buff.toLowerCase();
         if (buff.indexOf("get gps") > 1) {
           getGps();
-          String sms_data;
-          sms_data = "GPS Location Data\r";
+          String sms_data = "GPS Location Data\r";
           sms_data += "http://maps.google.com/maps?q=loc:";
           sms_data += latitude + "," + longitude;
-
           sendSms(sms_data);
         }
       }
     }
   }
-  else {
-  }
 }
 
-void getGps()
-{
-  // Can take up to 60 seconds
+void getGps() {
   boolean newData = false;
   for (unsigned long start = millis(); millis() - start < 2000;) {
     while (neogps.available()) {
@@ -201,45 +179,35 @@ void getGps()
     }
   }
 
-  if (newData)
-  {
+  if (newData) {
     latitude = String(gps.location.lat(), 6);
     longitude = String(gps.location.lng(), 6);
-    newData = false;
-  }
-  else {
+  } else {
     Serial.println("No GPS data is available");
     latitude = "";
     longitude = "";
   }
 
   Serial.print("Latitude= "); Serial.println(latitude);
-  Serial.print("Lngitude= "); Serial.println(longitude);
+  Serial.print("Longitude= "); Serial.println(longitude);
 }
 
-
-void sendAlert()
-{
-  String sms_data;
-  sms_data = "Accident Alert!!\r";
+void sendAlert() {
+  String sms_data = "Accident Alert!!\r";
   sms_data += "http://maps.google.com/maps?q=loc:";
   sms_data += latitude + "," + longitude;
-
   sendSms(sms_data);
 }
 
-void makeCall()
-{
-  Serial.println("calling....");
+void makeCall() {
+  Serial.println("Calling....");
   sim800.println("ATD" + EMERGENCY_PHONE + ";");
-  delay(20000); //20 sec delay
+  delay(20000); // 20 sec delay
   sim800.println("ATH");
-  delay(1000); //1 sec delay
+  delay(1000); // 1 sec delay
 }
 
-void sendSms(String text)
-{
-  //return;
+void sendSms(String text) {
   sim800.print("AT+CMGF=1\r");
   delay(1000);
   sim800.print("AT+CMGS=\"" + EMERGENCY_PHONE + "\"\r");
@@ -251,17 +219,15 @@ void sendSms(String text)
   Serial.println("SMS Sent Successfully.");
 }
 
-
 boolean SendAT(String at_command, String expected_answer, unsigned int timeout) {
-
   uint8_t x = 0;
   boolean answer = 0;
   String response;
   unsigned long previous;
-  while ( sim800.available() > 0) sim800.read();
 
+  while (sim800.available() > 0) sim800.read();
   sim800.println(at_command);
-
+  
   x = 0;
   previous = millis();
 
@@ -274,8 +240,8 @@ boolean SendAT(String at_command, String expected_answer, unsigned int timeout) 
         break;
       }
     }
-  }
-  while ((answer == 0) && ((millis() - previous) < timeout));
+  } while ((answer == 0) && ((millis() - previous) < timeout));
+  
   Serial.println(response);
   return answer;
 }
